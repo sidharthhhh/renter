bda7685e6455dfc4cf502914c206a10339fcf3cb
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2023-02-07 15:23:54 +0530

    conflict resolve

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require("mongoose");
-const plm  = require("passport-local-mongoose");
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const plm = require('passport-local-mongoose')
 
-mongoose.connect("mongodb://localhost/renter");
+mongoose.connect('mongodb://0.0.0.0/renter')
 
 var userSchema = mongoose.Schema({
   name: String,
-  userName:String,
+  userName: String,
   //user is rental (renting some property)
-  isRental:Boolean,
+  isRental: Boolean,
   contact: Number,
-  profileImg:String,
+  profileImg: String,
   profileDescription: String,
-  address:{
-    type:String,
-    default:""
+  address: {
+    type: String,
+    default: '',
   },
-  properties:[{
-    type:mongoose.Types.ObjectId,
-    ref:Properties
-  }],
-  idProof:{
-    type:{
-      idType:String,
-      idNumber:String // pancard no. contains character
-    }
-  }
-});
+  properties: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'Properties',
+    },
+  ],
+  idProof: {
+    type: {
+      idType: String,
+      idNumber: String, // pancard no. contains character
+    },
+  },
+})
 
-userSchema.plugin(plm);
-module.exports = mongoose.model("user" , userSchema);
+userSchema.plugin(plm)
+module.exports = mongoose.model('user', userSchema)
