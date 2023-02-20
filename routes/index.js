3de001bd9953ae1d420ec8c13803ce3e078cefac
Anshita-02 <anshitamethi732@gmail.com>
2023-02-20 12:55:54 +0530

    propertyrouter

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,34 @@ const config = require("../config/config.js");
 
 /* GET home page. */
 const localStrategy = require("passport-local");
-const properties = require('./properties.js');
 passport.use(new localStrategy(userModel.authenticate()));
-const UserImageUpload = multer({storage:config});
+const UserImageUpload = multer({storage:config.UserImageStorage});
+const ProductImageUpload = multer({storage: config.ProductImageStorage});
+
+
+// router.get('/', function(req, res, next) {
+//   var details = {username:"", profilepic:""};
+//   res.render('index' , details);
+// });
 
 
 
-router.get('/', function(req, res, next) {
-  var details = {username:"", profilepic:""};
-  res.render('index' , details);
+
+//index page
+router.get('/', function (req, res, next) {
+  if (req.isAuthenticated()) {
+    user.findOne({ email: req.user.username }, (err, user) => {
+      var details = { username: user.username, profilepic: user.profilepic }
+      res.render('index', details)
+    })
+  } else {
+    var details = { username: '', profilepic: '' }
+    res.render('index', details)
+  }
 });
 
+
+//user register
 router.post('/register' , function(req,res,next){
   var userDets = new userModel({
     name:req.body.name,
@@ -28,7 +45,7 @@ router.post('/register' , function(req,res,next){
 userModel.register(userDets , req.body.password)
 .then(function(registeredUser){
   passport.authenticate('local')(req,res,function(){
-    res.redirect('/success')
+    res.redirect('/')
   });
 });
 });
@@ -44,15 +61,15 @@ router.get('/login', function(req, res, next) {
 // router.get('/success' , function(req,res,next){
 //   res.send("login successfully");
 // })
-router.post('/success' , isLoggedIn, async function(req,res){
-  var user = await userModel.findOne({username: req.session.passport.user});
-  var details = {username: user.username , profilepic:user.profileImg};
-  res.render('index' , details);
-})
+// router.post('/success' , isLoggedIn, async function(req,res){
+//   var user = await userModel.findOne({username: req.session.passport.user});
+//   var details = {username: user.username , profilepic:user.profileImg};
+//   res.render('index' , details);
+// })
 
 router.post('/login' , passport.authenticate('local' , {
-  successRedirect: '/success',
-  failureRedirect: '/loginpage'
+  successRedirect: '/',
+  failureRedirect: '/login'
 }),function(req,res,next){});
 
 
@@ -117,38 +134,63 @@ router.post('/verify',isLoggedIn,  UserImageUpload.single('image'), async functi
 })
 
 
-//to find products
-router.get('/find/properties' ,isLoggedIn, async function(req,res){
+//to find products: for first page show first 6 elements
+router.get('/find/properties' , async function(req,res){
       var properties = await propertiesModel.find().limit(6);
       res.send(properties);
      
 })
 
-router.post('/upload/properties' , isLoggedIn, async function(req,res){
+//to find products on the basis of on which page u are
+router.get('/find/properties/:pageno' , async function(req,res){
+  var properties = await propertiesModel.find().skip(req.params.pageno*6).limit(6);
+  res.send(properties);
+})
+
+//to upload property
+router.post('/upload/property' , isLoggedIn,ProductImageUpload.array('images',4), async function(req,res){
   var user = userModel.findOne({username: req.session.passport.user});
   var data={
     ownerId: user._id,
     propertyDescription: req.body.propertyDescription,
     propertyAddress: req.body.propertyAddress,
-    LatitudeAndLongitude:{latitude:"", longitude:""},
+    // LatitudeAndLongitude:{latitude:"", longitude:""},
     price:req.body.price,
-    addOnAmenities:req.body.ammenities,
-    propertyType:req.body.type,
-    accessibilty:req.body.accessibility,
+    addOnAmenities:req.body.ammenities||[],
+    propertyType:req.body.proprtyType,
+    accessibility:req.body.accessibility,
     furnishedType:req.body.furnished,
     houseRules:req.body.houseRules,
     bedrooms:req.body.bedrooms,
     beds:req.body.beds,
     floor:req.body.floor,
-    status:req.body.status
+    status:req.body.status,
+    pics:req.files.map(elem=>elem.filename)
   }
   await propertiesModel.create(data);
-  res.send("You have succesfully uploaded your property");
+  // res.send("You have succesfully uploaded your property");
+ res.redirect('/profile')
 })
-//pagination router
 
-router.get('/upload/property' , function(req,res){
+//to display upload/property form
+router.get('/upload/property' , isLoggedIn, function(req,res){
   res.render("property");
+});
+
+//on click of room filter finding property on the basis of no. of rooms
+router.get('/filter/property/:roomno' , async function(req,res){
+  var rooms = req.params.roomno;
+  var properties = await propertiesModel.find({bedrooms: rooms});
+  res.send(properties);
+  res.send(rooms);
+})
+
+//on click of room filter finding property on the basis of price range
+router.get('/filter/property/:minprice/:maxprice', async function(req,res){
+    var minprice = req.params.minprice;
+    var maxprice = req.params.maxprice;
+    var properties = await propertiesModel.find({price:{$gt: minprice-1, $lt: maxprice+1}});
+    res.send(properties);
 })
 
 module.exports = router;
